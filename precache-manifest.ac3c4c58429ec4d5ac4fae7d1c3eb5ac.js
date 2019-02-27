173bb696bb54ed63877684b4040dd33a6afc0b31
Sephix <Sephix57@gmail.com>
2019-02-27 23:06:22 +0100

    Updates

---
+++ precache-manifest.ac3c4c58429ec4d5ac4fae7d1c3eb5ac.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/tetris/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "f6d59f904ce9f48790c9",
+    "url": "/tetris/static/css/main.9f02f25d.chunk.css"
+  },
+  {
+    "revision": "18d269b309cd4e2e4f13",
+    "url": "/tetris/static/js/runtime~main.18d269b3.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/tetris/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "cd83e6d00885dbaebbe2",
+    "url": "/tetris/static/js/2.cd83e6d0.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/tetris/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/tetris/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/tetris/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/tetris/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/tetris/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/tetris/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "f6d59f904ce9f48790c9",
+    "url": "/tetris/static/js/main.f6d59f90.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/tetris/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/tetris/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/tetris/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/tetris/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/tetris/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/tetris/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/tetris/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/tetris/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "cd83e6d00885dbaebbe2",
+    "url": "/tetris/static/css/2.a77eb190.chunk.css"
+  },
+  {
+    "revision": "da90eaecf91d2bdaa6b1e9ace6507b74",
+    "url": "/tetris/index.html"
+  }
+];
